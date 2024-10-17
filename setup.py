002c2570eb9462c2e21cdc147da76280dafa9afa
vprusso <vincentrusso1@gmail.com>
2024-10-17 18:44:29 -0400

    Updating tea-console.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     description="Set of helpers for developing console apps.",
     long_description=io.open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/alefnula/tea-console",
+    url="https://github.com/unitaryfund/tea-console",
     platforms=["Windows", "POSIX", "MacOSX"],
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -38,5 +38,4 @@ setup(
     license="Apache-2.0",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=io.open("requirements.txt").read().splitlines(),
 )
